1b791924a8b43c0ea368208cf60a35b5da6d5da7
Lungchien Yeh 葉龍乾 <thomas@sunplus.com.tw>
2025-12-15 09:45:13 +0800

    modify y=x^6  to y=x^5

--- func2.cpp
+++ func2.cpp
@@ -5,7 +5,7 @@ int main() {
     double x;
     std::cout << "Enter x: ";
     if (!(std::cin >> x)) return 0;
-    double y = std::pow(x, 6.0);
-    std::cout << "y = x^6 = " << y << std::endl;
+    double y = std::pow(x, 5.0);
+    std::cout << "y = x^5 = " << y << std::endl;
     return 0;
 }
